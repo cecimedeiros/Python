f844d5d969112a323cd09a318d74667d4168a4d9
Cecília Medeiros <98926823+cecimedeiros@users.noreply.github.com>
2023-03-24 15:49:51 -0300

    Create exercicioFloricultura.py

---
+++ exercicioFloricultura.py
@@ -0,0 +1,7 @@
+valor = float(input('digite o valor que você deseja gastar'))
+
+rosa = valor // 5.80
+tulipa = valor // 7.20
+
+print('a quantidade de rosas possíveis de comprar é %d' %rosa)
+print('a quantidade de tulipas possíveis de comprar é %d' %tulipa)
